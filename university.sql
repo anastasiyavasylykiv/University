82d2ba62ec58a1324b473291d95fd658e44fbc9d
anastasiya_vasylykiv <anastasiya.vasylykiv@gmail.com>
2018-04-22 22:39:27 +0300

    add create scheme

--- university.sql
+++ university.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `university` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `university`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: university
@@ -135,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-22 22:32:12
+-- Dump completed on 2018-04-22 22:38:28
