5bb497f53bb64f67fc6e6e6cf457cea4e2faab59
anastasiya_vasylykiv <anastasiya.vasylykiv@gmail.com>
2018-04-22 22:33:29 +0300

    new Database

--- university.sql
+++ university.sql
@@ -1,86 +1,138 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: university
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+--
+-- Table structure for table `degree`
+--
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `university` DEFAULT CHARACTER SET utf8 ;
-USE `university` ;
+DROP TABLE IF EXISTS `degree`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `degree` (
+  `id_degree` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_degree`),
+  UNIQUE KEY `id_degree_UNIQUE` (`id_degree`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `mydb`.`department`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `university`.`department` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `department_id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `department_name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB;
+--
+-- Dumping data for table `degree`
+--
 
+LOCK TABLES `degree` WRITE;
+/*!40000 ALTER TABLE `degree` DISABLE KEYS */;
+INSERT INTO `degree` VALUES (3,'assistant'),(2,'associate professor'),(1,'professor');
+/*!40000 ALTER TABLE `degree` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `mydb`.`degree`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `university`.`degree` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `degree_id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `degree_name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB;
+--
+-- Table structure for table `department`
+--
 
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `id_department` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `id_head` int(11) NOT NULL,
+  PRIMARY KEY (`id_department`),
+  UNIQUE KEY `id_departments_UNIQUE` (`id_department`),
+  KEY `id_head_idx` (`id_head`),
+  CONSTRAINT `id_head` FOREIGN KEY (`id_head`) REFERENCES `lector` (`id_lector`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `mydb`.`lector`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `university`.`lector` (
-  `id` INT NOT NULL,
-  `surname` VARCHAR(45) NOT NULL,
-  `lector_name` VARCHAR(45) NULL,
-  `subject` VARCHAR(45) NULL,
-  `degree_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `degree_id`),
-  UNIQUE INDEX `lector_id_UNIQUE` (`id` ASC),
-  INDEX `fk_lector_degree1_idx` (`degree_id` ASC),
-  CONSTRAINT `fk_lector_degree1`
-    FOREIGN KEY (`degree_id`)
-    REFERENCES `mydb`.`degree` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `department`
+--
 
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (1,'Faculty of Applied Mathematics',1),(2,'Faculty of Philology',2),(3,'Faculty of Economics',3);
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `mydb`.`department_has_lector`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `university`.`department_has_lector` (
-  `department_id` INT NOT NULL,
-  `lector_id` INT NOT NULL,
-  `lector_degree_id` INT NOT NULL,
-  INDEX `fk_department_has_lector_lector1_idx` (`lector_id` ASC, `lector_degree_id` ASC),
-  INDEX `fk_department_has_lector_department1_idx` (`department_id` ASC),
-  PRIMARY KEY (`department_id`, `lector_degree_id`, `lector_id`),
-  CONSTRAINT `fk_department_has_lector_department1`
-    FOREIGN KEY (`department_id`)
-    REFERENCES `mydb`.`department` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_department_has_lector_lector1`
-    FOREIGN KEY (`lector_id`)
-    REFERENCES `mydb`.`lector` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `lector`
+--
 
+DROP TABLE IF EXISTS `lector`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lector` (
+  `id_lector` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `second_name` varchar(45) NOT NULL,
+  `degree_id` int(11) NOT NULL,
+  `salary` int(11) NOT NULL,
+  PRIMARY KEY (`id_lector`),
+  UNIQUE KEY `id_lector_UNIQUE` (`id_lector`),
+  KEY `degree_id_idx` (`degree_id`),
+  CONSTRAINT `degree_id` FOREIGN KEY (`degree_id`) REFERENCES `degree` (`id_degree`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Dumping data for table `lector`
+--
+
+LOCK TABLES `lector` WRITE;
+/*!40000 ALTER TABLE `lector` DISABLE KEYS */;
+INSERT INTO `lector` VALUES (1,'Igor','Vozniak',1,8700),(2,'Vasyl','Komasiuk',1,13200),(3,'Ivan','Ivanov',3,3400),(4,'Oleh','Petrov',2,6200);
+/*!40000 ALTER TABLE `lector` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lector_department`
+--
+
+DROP TABLE IF EXISTS `lector_department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lector_department` (
+  `id_lector` int(11) NOT NULL,
+  `id_department` int(11) NOT NULL,
+  KEY `id_lector_idx` (`id_lector`),
+  KEY `id_department_idx` (`id_department`),
+  CONSTRAINT `id_department` FOREIGN KEY (`id_department`) REFERENCES `department` (`id_department`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_lector` FOREIGN KEY (`id_lector`) REFERENCES `lector` (`id_lector`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lector_department`
+--
+
+LOCK TABLES `lector_department` WRITE;
+/*!40000 ALTER TABLE `lector_department` DISABLE KEYS */;
+INSERT INTO `lector_department` VALUES (1,1),(2,1),(3,2),(3,3),(1,3),(4,2),(4,1);
+/*!40000 ALTER TABLE `lector_department` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-22 22:32:12
